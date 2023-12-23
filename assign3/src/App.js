c3091291e4ec1d6f3e791267e8b1406b178b52f8
javidalasgarli <alasgarlijavid@gmail.com>
2023-12-23 22:19:42 +0400

    Option to add new flash cards.

--- assign3/src/App.js
+++ assign3/src/App.js
@@ -4,10 +4,50 @@ import './app.css'
 
 function App() {
   const [flashcards, setFlashCards] = useState(SAMPLE_FLASHCARDS);
-  return (
-    <ListOfFlashCards flashcards={flashcards}/>
-  );
+  const handleAddFlashCard = () => {
+    const newFlashcard = {
+      id: flashcards.length + 1,
+      question: 'New Question',
+      answer: 'answer',
+      options: [
+        'answer 1',
+        'answer 2',
+        'answer 3'
+      ]
+    };
+    setFlashCards([...flashcards, newFlashcard]);
+  }
+
+
+    return (
+      <div>
+        <ListOfFlashCards flashcards={flashcards} />
+        <button onClick={handleAddFlashCard}>Add Flashcard</button>
+      </div>);
 }
+
+
+// function App() {
+//   const [flashcards, setFlashCards] = useState(SAMPLE_FLASHCARDS);
+
+//   const handleAddFlashCard = () => {
+//     const newFlashcard = {
+//       id: flashcards.length + 1,
+//       question: 'New Question',
+//       answer: 'New Answer',
+//       options: ['Option 1', 'Option 2', 'Option 3'],
+//     };
+
+//     setFlashCards([...flashcards, newFlashcard]);
+//   };
+
+//   return (
+//     <div>
+//       <ListOfFlashCards flashcards={flashcards} />
+//       <button onClick={handleAddFlashCard}>Add Flashcard</button>
+//     </div>
+//   );
+// }
 
 const SAMPLE_FLASHCARDS = [
   {
